e726be55533c470920cfe7126463995661f2b180
Max Hiroyuki Ueda <max.ueda@mac-desenv02-ios.local>
2019-08-08 21:09:16 -0300

    - SwiftLint stuff

--- Source/HFCardCollectionViewLayoutDelegate.swift
+++ Source/HFCardCollectionViewLayoutDelegate.swift
@@ -9,36 +9,42 @@
 import UIKit
 
 /// Extended delegate.
-@objc public protocol HFCardCollectionViewLayoutDelegate : UICollectionViewDelegate {
-    
+@objc public protocol HFCardCollectionViewLayoutDelegate: UICollectionViewDelegate {
+
     /// Asks if the card at the specific index can be revealed.
     /// - Parameter collectionViewLayout: The current HFCardCollectionViewLayout.
     /// - Parameter canRevealCardAtIndex: Index of the card.
-    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, canRevealCardAtIndex index: Int) -> Bool
-    
+    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout,
+                                                 canRevealCardAtIndex index: Int) -> Bool
+
     /// Asks if the card at the specific index can be Unrevealed.
     /// - Parameter collectionViewLayout: The current HFCardCollectionViewLayout.
     /// - Parameter canUnrevealCardAtIndex: Index of the card.
-    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, canUnrevealCardAtIndex index: Int) -> Bool
-    
+    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout,
+                                                 canUnrevealCardAtIndex index: Int) -> Bool
+
     /// Feedback when the card at the given index will be revealed.
     /// - Parameter collectionViewLayout: The current HFCardCollectionViewLayout.
     /// - Parameter didRevealedCardAtIndex: Index of the card.
-    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, willRevealCardAtIndex index: Int)
-    
+    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout,
+                                                 willRevealCardAtIndex index: Int)
+
     /// Feedback when the card at the given index was revealed.
     /// - Parameter collectionViewLayout: The current HFCardCollectionViewLayout.
     /// - Parameter didRevealedCardAtIndex: Index of the card.
-    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, didRevealCardAtIndex index: Int)
-    
+    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout,
+                                                 didRevealCardAtIndex index: Int)
+
     /// Feedback when the card at the given index will be Unrevealed.
     /// - Parameter collectionViewLayout: The current HFCardCollectionViewLayout.
     /// - Parameter didUnrevealedCardAtIndex: Index of the card.
-    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, willUnrevealCardAtIndex index: Int)
-    
+    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout,
+                                                 willUnrevealCardAtIndex index: Int)
+
     /// Feedback when the card at the given index was Unrevealed.
     /// - Parameter collectionViewLayout: The current HFCardCollectionViewLayout.
     /// - Parameter didUnrevealedCardAtIndex: Index of the card.
-    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, didUnrevealCardAtIndex index: Int)
-    
+    @objc optional func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout,
+                                                 didUnrevealCardAtIndex index: Int)
+
 }
